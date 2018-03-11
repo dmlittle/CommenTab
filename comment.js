9caef36c4685b3210c327d334356f9dd8f8286ae
Donald Little <donald@lob.com>
2018-03-10 22:28:24 -0800

    feat(persistance): persist comments throughout page navigation within tab

--- comment.js
+++ comment.js
@@ -1,78 +1,154 @@
 (function () {
 'use strict';
 
-function getCommentColor() {
-    var random = Math.random();
-    if (random > 0.66) {
-        return 'commentab_yellow';
-    } else if (random > 0.33) {
-        return 'commentab_green';
-    } else {
-        return 'commentab_blue';
+function generateComment(details) {
+
+    function generateId() {
+        var prefix = 'comment_';
+        var length = 6;
+
+        return prefix + Math.random().toString(16).substr(2, length);
     }
-}
 
-var comment = document.createElement('div')
-comment.classList.add('commentab');
-comment.classList.add(getCommentColor());
-comment.style.top = '50px';
-comment.style.left = '50px';
-
-var header = document.createElement('div');
-header.classList.add('header');
-
-var closeBtn = document.createElement('span');
-closeBtn.classList.add('closeBtn');
-
-var minimizeBtn = document.createElement('span');
-minimizeBtn.classList.add('minimizeBtn');
-
-var body = document.createElement('div');
-body.classList.add('body');
-
-var textarea = document.createElement('textarea');
-textarea.autocomplete = "off";
-textarea.autocorrect = "off";
-textarea.autocapitalize = "off";
-textarea.spellcheck = "false";
-
-minimizeBtn.onclick = function () {
-    if (body.style.display === 'none'){
-        return body.style.display = 'block';
-    } else {
-        return body.style.display = 'none';
+    function generateCommentColor() {
+        var random = Math.random();
+        if (random > 0.66) {
+            return 'commentab_yellow';
+        } else if (random > 0.33) {
+            return 'commentab_green';
+        } else {
+            return 'commentab_blue';
+        }
     }
-};
 
-closeBtn.onclick = function () {
-    return comment.remove();
-};
+    function updateCommentStorage () {
+        var key = `${TAB_ID}_${COMMENT_ID}`;
+        var updateStorage = {};
 
-comment.addEventListener('mousedown', function(event) {
-    var element = this;
-    var initX = element.offsetLeft;
-    var initY = element.offsetTop;
-    var mousePressX = event.clientX;
-    var mousePressY = event.clientY;
+        updateStorage[key] = {
+            id: COMMENT_ID,
+            value: textarea.value,
+            display: body.style.display,
+            color: COMMENT_COLOR,
+            position: {
+                top: comment.style.top,
+                left: comment.style.left
+            }
+        };
 
-    var repositionElement = function (event) {
-        element.style.left = initX + event.clientX - mousePressX + 'px';
-        element.style.top = initY + event.clientY - mousePressY + 'px';
+        chrome.storage.local.set(updateStorage);
+    }
+
+    var COMMENT_ID = details && details.id || generateId();
+    var COMMENT_COLOR = details && details.color || generateCommentColor();
+
+    var comment = document.createElement('div')
+    comment.classList.add('commentab');
+    comment.classList.add(COMMENT_COLOR);
+    comment.style.top = details && details.position && details.position.top || '50px';
+    comment.style.left = details && details.position && details.position.left || '50px';
+
+    var header = document.createElement('div');
+    header.classList.add('header');
+
+    var closeBtn = document.createElement('span');
+    closeBtn.classList.add('closeBtn');
+
+    var minimizeBtn = document.createElement('span');
+    minimizeBtn.classList.add('minimizeBtn');
+
+    var body = document.createElement('div');
+    body.classList.add('body');
+    body.style.display = details && details.display || 'block';
+
+    var textarea = document.createElement('textarea');
+    textarea.value = details && details.value || '';
+    textarea.autocomplete = "off";
+    textarea.autocorrect = "off";
+    textarea.autocapitalize = "off";
+    textarea.spellcheck = "false";
+
+    chrome.storage.local.get(TAB_ID, function (data) {
+        var comments = data[TAB_ID];
+        if (COMMENT_ID in comments) {
+            return;
+        }
+
+        var updateStorage = {};
+        updateStorage[TAB_ID] = comments.concat(COMMENT_ID);
+
+        chrome.storage.local.set(updateStorage);
+    });
+
+    minimizeBtn.onclick = function () {
+        if (body.style.display === 'none'){
+            body.style.display = 'block';
+        } else {
+            body.style.display = 'none';
+        }
+
+        updateCommentStorage();
     };
 
-    window.addEventListener('mousemove', repositionElement, false);
+    closeBtn.onclick = function () {
+        chrome.storage.local.remove(`${TAB_ID}_${COMMENT_ID}`);
+        chrome.storage.local.get(TAB_ID, function (data) {
+            var comments = data[TAB_ID];
 
-    window.addEventListener('mouseup', function() {
-      window.removeEventListener('mousemove', repositionElement, false);
+            var updateStorage = {};
+            updateStorage[TAB_ID] = comments.filter(function (comment_id) { return comment_id !== COMMENT_ID });
+
+            chrome.storage.local.set(updateStorage);
+        });
+
+        return comment.remove();
+    };
+
+    textarea.onkeyup = function () { 
+        updateCommentStorage();
+    }
+
+    comment.addEventListener('mousedown', function(event) {
+        var element = this;
+        var initX = element.offsetLeft;
+        var initY = element.offsetTop;
+        var mousePressX = event.clientX;
+        var mousePressY = event.clientY;
+
+        var repositionElement = function (event) {
+            element.style.left = initX + event.clientX - mousePressX + 'px';
+            element.style.top = initY + event.clientY - mousePressY + 'px';
+
+            updateCommentStorage();
+        };
+
+        window.addEventListener('mousemove', repositionElement, false);
+
+        window.addEventListener('mouseup', function() {
+          window.removeEventListener('mousemove', repositionElement, false);
+        }, false);
+
     }, false);
 
-}, false);
+    header.appendChild(closeBtn);
+    header.appendChild(minimizeBtn);
+    body.appendChild(textarea);
+    comment.appendChild(header);
+    comment.appendChild(body);
 
-header.appendChild(closeBtn);
-header.appendChild(minimizeBtn);
-body.appendChild(textarea);
-comment.appendChild(header);
-comment.appendChild(body);
+    document.body.appendChild(comment);
+}
 
-document.body.appendChild(comment);
+if (ACTION === 'create') {
+    generateComment();
+} else if (ACTION === 'restore') {
+    chrome.storage.local.get(TAB_ID, function (data) {
+        var comments = data[TAB_ID].map(function (comment_id) { return `${TAB_ID}_${comment_id}`; });
+
+        chrome.storage.local.get(comments, function (comments) {
+            Object.values(comments).forEach(function (c) { generateComment(c); })
+        })
+    });
+
+}
 })();
--- index.js
+++ index.js
@@ -12,15 +12,40 @@ chrome.contextMenus.create({
   contexts: ['all']
 });
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
+chrome.contextMenus.onClicked.addListener(async function (info, tab) {
   if (info.menuItemId === 'commentabCreate' && tab) {
-    chrome.tabs.executeScript(tab.id, { file: 'comment.js' });
+    chrome.tabs.executeScript(tab.id, { code: `var TAB_ID = 'tab_${tab.id}'; var ACTION = 'create';` }, function () {
+      chrome.tabs.executeScript(tab.id, { file: 'comment.js' });
+    });
   }
+});
+
+chrome.tabs.onCreated.addListener(function (tab) {
+  var TAB_ID = `tab_${tab.id}`;
+
+  var updateStorage = {};
+  updateStorage[TAB_ID] = [];
+
+  chrome.storage.local.set(updateStorage);
+});
+
+chrome.tabs.onRemoved.addListener(function (tab_id) {
+  var TAB_ID = `tab_${tab_id}`;
+
+  chrome.storage.local.get(TAB_ID, function (data) {
+    var comments = data[TAB_ID].map(function (comment_id) { return `${TAB_ID}_${comment_id}`; });
+
+    chrome.storage.local.remove([TAB_ID].concat(comments));
+  });
 });
 
 chrome.tabs.onCreated.addListener(load_css);
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
   if (info.status == 'complete') {
     load_css(tab);
+
+    chrome.tabs.executeScript(tab.id, { code: `var TAB_ID = 'tab_${tab.id}'; var ACTION = 'restore';` }, function () {
+      chrome.tabs.executeScript(tab.id, { file: 'comment.js' });
+    });
   }
 });
